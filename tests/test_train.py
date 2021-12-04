02e4706e6d914fee8be1ecdbdd7bd443bccf558d
yashshinge <yashshinge@gmail.com>
2021-12-03 23:26:36 -0600

    viz v1

--- tests/test_train.py
+++ tests/test_train.py
@@ -3,4 +3,4 @@ x = [math.sin(i/20)+i/300 for i in range(600)]
 from uniplot import plot
 x = [1, 2]
 y = [10, 20]
-plot(y, x, x_min=1, x_gridlines=[-1], x_max=3, y_max=100, lines=True, color=True, title="Test accuracy v/s Epochs")
+plot(y, x, x_min=1, x_gridlines=[-1], x_max=3, y_max=100, lines=True, title="Test accuracy v/s Epochs")
